d6dd5589e5d10d71bdaa74be37d27f20be786f18
RenardRouge890 <fer_noguez890@outlook.com>
2023-03-08 09:32:59 -0600

    Se módifico la Lista de Libros

--- src/views/LibrosView.vue
+++ src/views/LibrosView.vue
@@ -1,39 +1,43 @@
 <template>
-    <div>
-      <navigation/>
-      <h1>Libros Registrados</h1>
-      <v-data-table
-        :headers="headers"
-        :items="libros"
-        class="elevation-2"
-      ></v-data-table>
-    </div>
-  </template>
-  <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          { text: 'Id del Libro', value: 'libreriaMateriaId' },
-          { text: 'Titulo del Libro', value: 'titulo' },
-          { text: 'Fecha de Publicación del Libro', value: 'fechaPublicacion' },
-          { text: 'Guid del Autor Libro', value: 'autorLibro' },
-        ],
-        libros: [],
-        search: '',
-        pagination: {},
-        selected: []
-      }
-    },
-    created () {
-      this.mostrarLibros()
-    },
-    methods: {
-      mostrarLibros () {
-        fetch('https://localhost:44398/api/Libros/')
-          .then((response) => response.json())
-          .then((data) => (this.libros = data))
+  <div>
+    <navigation/>
+    <h1>Libros Registrados</h1>
+    <v-data-table
+      :headers="headers"
+      :items="libros"
+      class="elevation-2"
+    ></v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      headers: [
+        { text: 'Guid del Libro', value: 'libreriaMateriaId' },
+        { text: 'Titulo del Libro', value: 'titulo' },
+        { text: 'Fecha de Publicación del Libro', value: 'fechaPublicacion' },
+        { text: 'Guid del Autor Libro', value: 'autorLibro' },
+      ],
+      libros: [],
+      search: '',
+      pagination: {},
+      selected: []
+    }
+  },
+  async created () {
+    await this.mostrarLibros()
+  },
+  methods: {
+    async mostrarLibros () {
+      try {
+        const response = await fetch('https://localhost:44398/api/Libros/');
+        const data = await response.json();
+        this.libros = data;
+      } catch (error) {
+        console.error(error);
       }
     }
   }
-  </script>
+}
+</script>
